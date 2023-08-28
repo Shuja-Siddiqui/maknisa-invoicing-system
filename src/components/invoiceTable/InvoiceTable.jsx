f134e7b767c7dd1eb0fca2d57bddea0f890aab7f
Abdul Raheem <shuja0094@gmail.com>
2023-08-28 12:45:29 +0500

    SEARCH FIELD ADDED

--- src/components/invoiceTable/InvoiceTable.jsx
+++ src/components/invoiceTable/InvoiceTable.jsx
@@ -10,7 +10,6 @@ import {
   TablePagination,
 } from "@mui/material";
 import styled from "styled-components";
-import { Delete, RemoveRedEye } from "@mui/icons-material";
 
 const StyledTableCell = styled(TableCell)({
   borderBottom: "1px solid #f98e0a",
@@ -24,6 +23,7 @@ const WhiteTextTableCell = styled(TableCell)({
 export const InvoiceTable = ({ rows, headings, Actions }) => {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -33,12 +33,36 @@ export const InvoiceTable = ({ rows, headings, Actions }) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
-
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+    setPage(0); // Reset the page when the search query changes
+  };
   const emptyRows =
     rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
-
+  const filteredRows = rows.filter((row) =>
+    headings.some((heading) =>
+      row[heading].toString().toLowerCase().includes(searchQuery.toLowerCase())
+    )
+  );
   return (
     <TableContainer component={Paper} sx={{ bgcolor: "#000000" }}>
+      <StyledTableCell
+        sx={{
+          borderBottom: "1px solid #f98e0a",
+          color: "#f98e0a",
+          width: "20%",
+        }}
+        align={"center"}
+      >
+        Search
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          placeholder="Search..."
+          style={{ marginLeft: "8px", padding: "2px" }}
+        />
+      </StyledTableCell>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
@@ -70,7 +94,7 @@ export const InvoiceTable = ({ rows, headings, Actions }) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows
+          {filteredRows
             .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
             .map((row, index) => (
               <TableRow key={index}>
