7052c4c01278ad60189b482e1df98a16f70f1222
Abdul Raheem <shuja0094@gmail.com>
2023-08-30 00:14:32 +0500

    updated api link

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,8 @@
 export const url =
   process.env.REACT_APP_NODE_ENV === "local"
     ? "http://localhost:5000/api"
-    : "https://vercel.com/shuja-siddiqui/maknisa-invoicing-system/api";
+    : "https://maknisa-invoicing-system-api.vercel.app/api";
 export const file_url =
   process.env.REACT_APP_NODE_ENV === "local"
     ? "http://localhost:5000"
-    : "https://vercel.com/shuja-siddiqui/maknisa-invoicing-system";
+    : "https://maknisa-invoicing-system-api.vercel.app";
